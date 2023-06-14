5591cf480a6564b3f7a48f2288dd9922c00d2618
Atuh Samuel <atuhsamuel96@gmail.com>
2023-06-14 02:51:40 +0100

    Updates

--- cilC5Aws/src/urls.py
+++ cilC5Aws/src/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, DeleteBucket
 
 app_name = "src"
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('delete/', DeleteBucket.as_view(), name='delete'),
 ]
--- cilC5Aws/src/views.py
+++ cilC5Aws/src/views.py
@@ -8,18 +8,30 @@ from django.views import View
 
 class IndexView(View):
     def get(self, request):
-
-
-        return render(request, "index.html")
-
-    def post(self, request):
         aws_access_key_id = settings.AWS_ACCESS_KEY_ID
         aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
 
         session = boto3.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
         s3 = session.client('s3')
+        response = s3.list_buckets()
+
+        # Retrieve the list of buckets
+        buckets = response['Buckets']
+        print(buckets)
+
+        return render(request, "index.html", {"buckets": buckets})
+
+    def post(self, request):
+        aws_access_key_id = settings.AWS_ACCESS_KEY_ID
+        aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
+
+        print(aws_access_key_id, aws_secret_access_key)
+
+        session = boto3.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+        s3 = session.client('s3')
 
         try:
+            print("In try")
             response = s3.create_bucket(
                 Bucket=request.POST.get('name').lower().replace(" ", "-"),
                 CreateBucketConfiguration={
@@ -28,8 +40,10 @@ class IndexView(View):
             )
 
             # TODO: Check if bucket already exist
+            print(response['ResponseMetadata']['HTTPStatusCode'])
 
             if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                print("Got here")
                 send_mail(
                     'S3 Bucket Created',
                     'Your Bucket was created successfully!',
@@ -39,4 +53,27 @@ class IndexView(View):
                 )
                 return JsonResponse({'message': 'Your Bucket was created successfully!'})
         except:
+            print("In except")
             return JsonResponse({'message': 'Error encountered'})
+
+
+class DeleteBucket(View):
+    def post(self, request):
+        aws_access_key_id = settings.AWS_ACCESS_KEY_ID
+        aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
+
+        session = boto3.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+        print(request.POST.get("bucket"))
+
+        # Create an S3 client using the session
+        s3 = session.client('s3')
+
+        # Specify the bucket name to delete
+        bucket_name = request.POST.get("bucket")
+
+        # Delete the bucket
+        s3.delete_bucket(Bucket=bucket_name)
+
+        print(f"Bucket '{bucket_name}' deleted successfully.")
+        return JsonResponse({'message': f"Bucket '{bucket_name}' deleted successfully."})
